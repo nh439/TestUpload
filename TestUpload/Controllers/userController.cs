c9c33a6b8cd9ca7c179c3df3af772ba25667c720
nh439 <nithi.handsome@gmail.com>
2021-09-30 16:16:28 +0700

    XS

---
+++ TestUpload/Controllers/userController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestUpload.Controllers
+{
+    public class userController : Controller
+    {
+        [HttpGet("/user")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
